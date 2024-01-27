45e204fa690a49b9a3718a80bef550e65d6b2265
ZulfiPy <zulfizupy@gmail.com>
2024-01-27 07:44:27 +0200

    EMPLOYEE Mongo Model

---
+++ model/Employee.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const employeeSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    birthDate: {
+        type: String,
+        required: true
+    },
+    roles: {
+        User: {
+            type: Number,
+            default: 2001
+        },
+        Editor: Number,
+        Admin: Number
+    },
+    refreshToken: [String]
+});
+
+module.exports = mongoose.model('Employee', employeeSchema);
