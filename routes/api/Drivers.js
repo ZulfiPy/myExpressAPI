40d7ca4244665a5687d205873af7ad9d0b97e7b2
ZulfiPy <zulfizupy@gmail.com>
2024-01-28 07:34:21 +0200

    verifyRoles middleware setup

--- routes/api/Drivers.js
+++ routes/api/Drivers.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const verifyRoles = require('../../middleware/verifyRoles');
+const ROLES_LIST = require('../../config/roles_list');
 
 const driversController = require('../../controllers/driversController');
 
 router.route('/')
-    .get(driversController.getAllDrivers)
-    .post(driversController.createDriver)
-    .put(driversController.updateDriver)
-    .delete(driversController.deleteDriver)
+    .get(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor, ROLES_LIST.User), driversController.getAllDrivers)
+    .post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor, ROLES_LIST.User), driversController.createDriver)
+    .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), driversController.updateDriver)
+    .delete(verifyRoles(ROLES_LIST.Admin), driversController.deleteDriver)
 
 router.route('/:id')
-    .get(driversController.getDriver)
-    .put(driversController.updateDriver)
-    .delete(driversController.deleteDriver)
+    .get(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor, ROLES_LIST.User), driversController.getDriver)
+    .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), driversController.updateDriver)
+    .delete(verifyRoles(ROLES_LIST.Admin), driversController.deleteDriver)
 module.exports = router;
--- routes/api/Employees.js
+++ routes/api/Employees.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const employeeController = require('../../controllers/employeesController');
+const verifyRoles = require('../../middleware/verifyRoles');
+const ROLES_LIST = require('../../config/roles_list');
 
 router.route('/')
-    .get(employeeController.getAllEmployees)
-    .put(employeeController.updateEmployee)
-    .delete(employeeController.deleteEmployee)
+    .get(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor, ROLES_LIST.User), employeeController.getAllEmployees)
+    .put(verifyRoles(ROLES_LIST.Admin), employeeController.updateEmployee)
+    .delete(verifyRoles(ROLES_LIST.Admin), employeeController.deleteEmployee)
 
 router.route('/:id')
-    .get(employeeController.getEmployee)
-    .put(employeeController.updateEmployee)
-    .delete(employeeController.deleteEmployee)
+    .get(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor, ROLES_LIST.User), employeeController.getEmployee)
+    .put(verifyRoles(ROLES_LIST.Admin), employeeController.updateEmployee)
+    .delete(verifyRoles(ROLES_LIST.Admin), employeeController.deleteEmployee)
 
 module.exports = router;
