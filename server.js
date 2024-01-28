486e22b3f1208a7099ca4bbca0e81a99e46d4975
ZulfiPy <zulfizupy@gmail.com>
2024-01-28 06:11:32 +0200

    verifyJWT middleware added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express()
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3500;
 const connectDB = require('./config/dbConn');
+const verifyJWT = require('./middleware/verifyJWT');
 
 app.get('/', (req, res) => {
     res.send('hello world');
@@ -14,11 +15,14 @@ connectDB();
 
 app.use(express.json())
 
+// routes
+app.use('/register', require('./routes/register'));
+app.use('/auth', require('./routes/auth'));
+
 // file iis defined as Drivers.js and Employee.js but for some reason it requires to import it as drivers.js and employee.js
+app.use(verifyJWT);
 app.use('/api/drivers', require('./routes/api/drivers'));
 app.use('/api/employees', require('./routes/api/Employees'));
-
-app.use('/register', require('./routes/register'));
 
 mongoose.connection.once('open', () => {
     console.log('connected to MongoDB');
