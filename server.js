059791c765d3b7fb8e5ca29848b537ef677e739b
ZulfiPy <zulfizupy@gmail.com>
2024-01-29 06:51:13 +0200

    /logout route use

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(cookieParser());
 app.use('/register', require('./routes/register'));
 app.use('/auth', require('./routes/auth'));
 app.use('/refresh', require('./routes/refresh'));
+app.use('/logout', require('./routes/logout'));
 
 // file iis defined as Drivers.js and Employee.js but for some reason it requires to import it as drivers.js and employee.js
 app.use(verifyJWT);
