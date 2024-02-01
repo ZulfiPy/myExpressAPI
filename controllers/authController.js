ed8caa53bcb4881c69d5f4395c4566ebc5c45815
ZulfiPy <zulfizupy@gmail.com>
2024-02-01 16:43:57 +0200

    commets added to auth logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,30 +4,32 @@ const jwt = require('jsonwebtoken');
 
 const handleLogin = async (req, res) => {
     console.log('handleLogin')
-    // extract the cookies from req.cookies
+
+    // Extract cookies from the request, which may contain a JWT token from a previous session
     const cookies = req.cookies;
     console.log(`cookies avaiable at`, JSON.stringify(cookies));
+
     // destruct username and password
     const { username, password } = req.body;
 
-    // username or password not provided
+    // Return a 400 Bad Request if username or password is not provided
     if (!username || !password) return res.status(400).json({ 'message': 'some of data is missing.' })
 
-    // find employee by the username
+    // Find an employee in the database by the provided username
     const foundEmployee = await Employee.findOne({ username }).exec();
 
-    // employee not found
+    // If the employee is not found, return a 401 Unauthorized status
     if (!foundEmployee) return res.status(401).json({ 'message': `Employee by username ${username} is not found in the database` });
 
-    // compare password with hashed password
+    // Compare the provided password with the stored hashed password
     const match = await bcrypt.compare(password, foundEmployee.password);
 
-    // correct username and password provided
+    // If the password matches
     if (match) {
-        // extract the values(roles) and stored them into the list
+        // Extract roles from the employee's record
         const roles = Object.values(foundEmployee.roles).filter(Boolean);
 
-        // create accessToken
+        // Create an access token with the employee's username and roles, set to expire in 5 minutes
         const accessToken = jwt.sign(
             {
                 "UserInfo": {
@@ -39,7 +41,7 @@ const handleLogin = async (req, res) => {
             { expiresIn: "5m" }
         );
 
-        // create refreshToken
+        // Create a refresh token with the employee's username, set to expire in 1 hour
         const newRefreshToken = jwt.sign(
             {
                 "username": foundEmployee.username
@@ -49,38 +51,39 @@ const handleLogin = async (req, res) => {
             { expiresIn: "1h" }
         );
 
-        // Check for existing JWT in cookies (first login/new device || 1++ login/old device)
+        // Prepare an array of refresh tokens, excluding any existing JWT token in the cookies
         let newRefreshTokenArray =
             !cookies?.jwt
                 ? foundEmployee.refreshToken
                 : foundEmployee.refreshToken.filter(rt => rt !== cookies.jwt);
-       console.log('newRefreshTokenArray', newRefreshTokenArray)
-        // if JWT cookie exists, check its validity in the DB
+        console.log('newRefreshTokenArray', newRefreshTokenArray);
+
+        // If a JWT cookie exists, verify its validity in the database
         if (cookies?.jwt) {
             const refreshToken = cookies.jwt;
             const foundToken = await Employee.findOne({ refreshToken }).exec();
-            // if JWT cookie doesn't exist in the database, clear the array of refresh tokens (indicating potential misuse)
+            // If the JWT cookie doesn't match any record in the database, clear all refresh tokens (indicating potential misuse)
             if (!foundToken) {
                 newRefreshTokenArray = [];
             }
-            // clear old JWT cookie
+            // Clear the old JWT cookie from the client
             // when you test backend with Thunder Client or Postman, comment or clear secure: true
-            res.clearCookie('jwt', { httpOnly: true,  sameSite: 'none' }); // secure: true
+            res.clearCookie('jwt', { httpOnly: true, sameSite: 'none' }); // secure: true
         }
 
-        // update employee refreshToken array and save the record
+        // Update the employee's record with the new set of refresh tokens and save it
         foundEmployee.refreshToken = [...newRefreshTokenArray, newRefreshToken];
         const result = await foundEmployee.save();
         console.log("result", result);
 
-        // Set new JWT refreshToken to cookies
+        // Set the new refresh token in the client's cookie
         // when you test backend with Thunder Client or Postman, comment or clear secure: true
         res.cookie('jwt', newRefreshToken, { httpOnly: true, sameSite: 'none', maxAge: 24 * 60 * 60 * 1000 }); // secure: true, 
 
-        // return new accessToken to the client
-        return res.json({accessToken});
+        // Return the new access token to the client
+        return res.json({ accessToken });
     } else {
-        // incorrect password provided
+        // If the password doesn't match, return a 401 Unauthorized status
         return res.status(401).json({ 'message': 'incorrect password, please try again' });
     }
 }
